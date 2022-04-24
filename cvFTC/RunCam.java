4efd533dfeec071445d7358e9c685bca22720d2d
tejusk2 <92613406+tejusk2@users.noreply.github.com>
2022-04-24 08:33:18 -0500

    Update RunCam.java

--- cvFTC/RunCam.java
+++ cvFTC/RunCam.java
@@ -1,1 +1,96 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.opmode.ConceptCV;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import java.io.IOException;
+
+@Autonomous(name="ContourTester",group="Linear OpMode")
+public class contourTester extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+      //THIS CODE RUNS CONTOUR PIPELINE
+      //JUST INITIALIZES EVERYTHING SHOULD BE EASY
+      
+      
+      
+      
+      
+      
+        OpenCvWebcam webcam;
+        int test = 1;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+        //OpenCV Pipeline
+
+        contourPipeline myPipeline = new contourPipeline();
+        webcam.setPipeline(myPipeline);
+
+        OpenCvWebcam finalWebcam = webcam;
+        finalWebcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                finalWebcam.startStreaming(1920 ,1080, OpenCvCameraRotation.UPSIDE_DOWN);
+                telemetry.addData("Initialization passed ", test);
+                telemetry.update();
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Init Failed ", errorCode);
+                telemetry.update();
+
+            }
+            
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        waitForStart();
+
+
+        while(opModeIsActive()) {
+            int midpoint = myPipeline.getMidpoint();
+
+            int location = myPipeline.getLocation();
+
+
+            telemetry.addData("Midpoint: ", midpoint);
+            telemetry.addData("Location: ",location);
+
+            telemetry.update();
+           
+
+
+            finalWebcam.stopStreaming();
+
+
+        }
+
+
+
+    }
+}
 
